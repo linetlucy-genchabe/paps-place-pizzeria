ac975370a563fe971d3942ef5afb2196ebb8f27a
linetlucy genchabe <linetlucy21@gmail.com>
2022-03-19 06:05:58 +0300

    add switch and if conditions

--- js/scripts.js
+++ js/scripts.js
@@ -18,3 +18,50 @@ $(document).ready(function() {
                             ptopping.push($(this).val());
                         });
                         console.log(ptopping.join(", "));
+                        switch (psize) {
+                            case "0":
+                                price = 0;
+                                break;
+                            case "large":
+                                price = 1200;
+                                console.log(price);
+                                break;
+                            case "medium":
+                                price = 850;
+                                console.log("The price is " + price);
+                                break;
+                            case "small":
+                                price = 600;
+                                console.log(price);
+                            default:
+                                console.log("error");
+                        }
+                        switch (pcrust) {
+                            case "0":
+                                crust_price = 0;
+                                break;
+                            case "Crispy":
+                                crust_price = 200;
+                                break;
+                            case "Stuffed":
+                                crust_price = 250;
+                                break;
+                            case "Gluten-free":
+                                crust_price = 180;
+                                break;
+                            default:
+                                console.log("No price");
+                        }
+                        let topping_value = ptopping.length * 50;
+                        console.log("toppins value" + topping_value);
+                        if ((psize == "0") && (pcrust == "0")) {
+                            console.log("nothing selected");
+                            $("button.proceed").show();
+                            $("#information").show();
+                            $("div.choise").hide();
+                            alert("Please select pizza size and crust");
+                        } else {
+                            $("button.proceed").hide();
+                            $("#information").hide();
+                            $("div.choise").slideDown(1000);
+                        }
