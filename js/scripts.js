49cb6e578cded7ed07af86edeb257af61831dc0c
linetlucy genchabe <linetlucy21@gmail.com>
2022-03-19 06:08:47 +0300

    add pizza function

--- js/scripts.js
+++ js/scripts.js
@@ -75,3 +75,62 @@ $(document).ready(function() {
                         $("#pizzacrust").html($("#crust option:selected").val());
                         $("#pizzatopping").html(ptopping.join(", "));
                         $("#totals").html(total);
+                        $("button.addPizza").click(function() {
+                            let pname = $(".name option:selected").val();
+                            let psize = $("#size option:selected").val();
+                            let pcrust = $("#crust option:selected").val();
+                            let ptopping = [];
+                            $.each($("input[name='toppings']:checked"), function() {
+                                ptopping.push($(this).val());
+                            });
+                            console.log(ptopping.join(", "));
+                            switch (psize) {
+                                case "0":
+                                    price = 0;
+                                    break;
+                                case "large":
+                                    price = 1200;
+                                    console.log(price);
+                                    break;
+                                case "medium":
+                                    price = 850;
+                                    console.log("The price is " + price);
+                                    break;
+                                case "small":
+                                    price = 600;
+                                    console.log(price);
+                                default:
+                                    console.log("error");
+                            }
+                            switch (pcrust) {
+                                case "0":
+                                    crust_price = 0;
+                                    break;
+                                case "Crispy":
+                                    crust_price = 200;
+                                    break;
+                                case "Stuffed":
+                                    crust_price = 150;
+                                    break;
+                                case "Gluten-free":
+                                    crust_price = 180;
+                                    break;
+                                default:
+                                    console.log("No price");
+                            }
+                            let topping_value = ptopping.length * 50;
+                            console.log("toppins value" + topping_value);
+                            total = price + crust_price + topping_value;
+                            console.log(total);
+
+                            checkoutTotal = checkoutTotal + total;
+                            console.log(checkoutTotal);
+                            // constractor function
+                            var newOrder = new Getpizza(pname, psize, pcrust, ptopping, total);
+
+                            $("#ordersmade").append('<tr><td id="pizzaname">' + newOrder.name + '</td><td id="pizzasize">' + newOrder.size + '</td><td id="pizzacrust">' + newOrder.crust + '</td><td id="pizzatopping">' + newOrder.topping + '</td><td id="totals">' + newOrder.total + '</td></tr>');
+                            console.log(newOrder);
+
+
+
+                        });
